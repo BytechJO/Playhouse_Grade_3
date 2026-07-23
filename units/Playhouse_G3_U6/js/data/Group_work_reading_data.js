8c76f4a1d13208365a0e84bb5c00fb0b49d2b9c6
duha1dahamsheh <developer2@bytechjo.com>
2026-07-23 11:47:44 +0300

    update bath of imgs

--- units/Playhouse_G3_U6/js/data/Group_work_reading_data.js
+++ units/Playhouse_G3_U6/js/data/Group_work_reading_data.js
@@ -1,47 +1,48 @@
 var stereo_data = {
-    "audio":"",
-    "exist":false,
-    "bgColor_rgb":"rgb(242, 101, 33)",
-    "type":"text",
-    "playListData" : [
-        {
-          'audiourl': '../audios/demo.mp3',
-        },
-        {
-          'url': '',
-        },
-        {
-          'url': '',
-        },
-        {
-          'url': '',
-        }
-    ],
-  }
-var fillin_data = {    
-    "layout"                : 1,
-    "numinrow"              : [[1]],
-    "mainTitle"             : "../images/icons/conv_main_title.png",
-    "mainTitleIcon"         : "../images/pages/sb-icons/conv_main_title_icon.png",
-    "mainTitleIconPos"      : {"right": "100px"},
-    "mainTitleAudio"        : "../audios/page_49/CONVERSATION.mp3",
-    "subTitleTextLeft"      : '<span class="orange_text">2</span> Pair work.',
-    "subTitleTextRight"     : '<span class="orange_text">In pairs, practise the conversation. Then try to use the words below to make a new conversation. Act it out for the class.</span>',
-    "subTitleIcons"         : [""],
-    "subTitleAudio"         : "../audios/new/p49pair.mp3",
-    "defaultAnswer"         : -1,
-    "leftList"              : '',
-    "image"                 : "",
-    "question"              :"",
-    "imageposition"         : "front",
-    "text"              : ["<div style='display: flex; flex-wrap: wrap; gap:25px;'>" +
-                           "<div><p>head of garlic</p><img src=../images/pages/page-2/Capture1.png height='150px'/></div>" +
-                           "<div><p>100 grams of spices</p><img src=../images/pages/page-2/Capture2.png height='150px'/></div>" +
-                           "<div><p>arge piece of cheese</p><img src=../images/pages/page-2/Capture3.png height='150px'/></div>" +
-                           "<div><p>two jars of jam</p><img src=../images/pages/page-2/Capture4.png height='150px'/></div>" +
-                           "</div>"],
-    "textAudios"        :[],
-    "questions": [{},
-        
-    ]
-}
+  audio: "",
+  exist: false,
+  bgColor_rgb: "rgb(242, 101, 33)",
+  type: "text",
+  playListData: [
+    {
+      audiourl: "../audios/demo.mp3",
+    },
+    {
+      url: "",
+    },
+    {
+      url: "",
+    },
+    {
+      url: "",
+    },
+  ],
+};
+var fillin_data = {
+  layout: 1,
+  numinrow: [[1]],
+  mainTitle: "../images/icons/conv_main_title.png",
+  mainTitleIcon: "../images/pages/sb-icons/conv_main_title_icon.png",
+  mainTitleIconPos: { right: "100px" },
+  mainTitleAudio: "../audios/page_49/CONVERSATION.mp3",
+  subTitleTextLeft: '<span class="orange_text">2</span> Pair work.',
+  subTitleTextRight:
+    '<span class="orange_text">In pairs, practise the conversation. Then try to use the words below to make a new conversation. Act it out for the class.</span>',
+  subTitleIcons: [""],
+  subTitleAudio: "../audios/new/p49pair.mp3",
+  defaultAnswer: -1,
+  leftList: "",
+  image: "",
+  question: "",
+  imageposition: "front",
+  text: [
+    "<div style='display: flex; flex-wrap: wrap; gap:25px;'>" +
+      "<div><p>head of garlic</p><img src=../images/pages/page-2/Capture1.png height='150px'/></div>" +
+      "<div><p>100 grams of spices</p><img src=../images/pages/page-2/Capture2.png height='150px'/></div>" +
+      "<div><p>arge piece of cheese</p><img src=../images/pages/page-2/Capture3.png height='150px'/></div>" +
+      "<div><p>two jars of jam</p><img src=../images/pages/page-2/Capture4.png height='150px'/></div>" +
+      "</div>",
+  ],
+  textAudios: [],
+  questions: [{}],
+};
